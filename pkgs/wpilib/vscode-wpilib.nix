0026d0e89b160bc70ac0fc30c12d5e93a4172641
NullCube <nullcub3@gmail.com>
2026-03-01 21:51:52 -0800

    vscode-wpilib: remove custom name

--- pkgs/wpilib/vscode-wpilib.nix
+++ pkgs/wpilib/vscode-wpilib.nix
@@ -1,8 +1,6 @@
 {
   vscode-utils,
-  allwpilibSources,
   fetchurl,
-  unzip,
   lib,
 }:
 vscode-utils.buildVscodeExtension rec {
@@ -14,17 +12,7 @@ vscode-utils.buildVscodeExtension rec {
   src = fetchurl {
     url = "https://github.com/wpilibsuite/vscode-wpilib/releases/download/v${version}/vscode-wpilib-${version}.vsix";
     hash = "sha256-Qj9CHQk8ODZiILGEbhBdBl5wLpAf9RsYa7avYT4ns7Y=";
-    # TODO: Once the version of nixpkgs in this flake is updated we should remove
-    # the custom `name` and the `unzip` nativeBuildInput
-    # See: https://github.com/NixOS/nixpkgs/commit/e24a734076ea21365bb618d63f5c9a70006dd196
-
-    # For compatibility older versions of nixpkgs
-    # The `*.vsix` file is in the end a simple zip file. Change the extension
-    # so that existing `unzip` hooks takes care of the unpacking.
-    name = "${name}.zip";
   };
-
-  nativeBuildInputs = [ unzip ];
 
   # VSCode Metadata
   vscodeExtPublisher = "wpilibsuite";
